33e57df9f3ee5af4e8556249bba91adfc40ab73a
chandu256 <81565889+chandu256@users.noreply.github.com>
2021-03-31 09:25:37 +0530

    Add files via upload

---
+++ biggest.java
@@ -0,0 +1,18 @@
+class biggest
+{
+public static void main(String a[])
+{
+int n1=Integer.parseInt(a[0]);
+int n2=Integer.parseInt(a[1]);
+int n3=Integer.parseInt(a[2]);
+if(n1>n2)
+{
+System.out.println("print the result="+n1);
+}
+else if(n1>n3)
+{
+System.out.println("print the result="+n3);
+}
+}
+}
+
